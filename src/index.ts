c2339e52e7d2a2bb721e268c17caef0a139a91fb
Sonu Ram <sonuram6960@gmail.com>
2020-07-29 23:19:45 +0530

    Use enum

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hello world");
+import fs from "fs";
+
+const matches = fs
+  .readFileSync("football.csv", { encoding: "utf-8" })
+  .split("\n")
+  .map((match) => match.split(","));
+
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (
+    (match[1] === "Man United" && match[5] === MatchResult.HomeWin) ||
+    (match[2] === "Man United" && match[5] === MatchResult.AwayWin)
+  )
+    manUnitedWins++;
+}
+
+console.log(`Man United won ${manUnitedWins} games.`);
