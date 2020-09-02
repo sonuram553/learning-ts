ad4467018d13ab54753faf3d7d14c967168212f9
Sonu Ram <sonuram6960@gmail.com>
2020-09-03 03:10:59 +0530

    Create routeBinder method to create factory-decorators for various http-methods
    - Create enums - Methods and MetadataKeys

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,5 +1,7 @@
 import "reflect-metadata";
 import { AppRouter } from "../../AppRouter";
+import { Methods } from "./Methods";
+import { MetadataKeys } from "./MetadataKeys";
 
 export const router = AppRouter.getInstance();
 
@@ -7,10 +9,20 @@ export function controller(routePrefix: string) {
   return function (target: Function) {
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
-      const path = Reflect.getMetadata("path", target.prototype, key);
+
+      const path = Reflect.getMetadata(
+        MetadataKeys.Path,
+        target.prototype,
+        key
+      );
+      const method: Methods = Reflect.getMetadata(
+        MetadataKeys.Method,
+        target.prototype,
+        key
+      );
 
       if (path) {
-        router.get(`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, routeHandler);
       }
     }
   };
--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,7 +1,15 @@
 import "reflect-metadata";
+import { Methods } from "./Methods";
+import { MetadataKeys } from "./MetadataKeys";
 
-export function get(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata("path", path, target, key);
+function routeBinder(method: string) {
+  return function (path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata(MetadataKeys.Path, path, target, key);
+      Reflect.defineMetadata(MetadataKeys.Method, method, target, key);
+    };
   };
 }
+
+export const get = routeBinder(Methods.Get);
+export const post = routeBinder(Methods.Post);
